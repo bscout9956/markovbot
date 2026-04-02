4c7b09a33e8136e4739a12d94152412336fbdca6
bscout9956 <bs.blackscout.bs@gmail.com>
2026-04-02 13:29:59 -0300

    Improve dataset script

--- dataset.py
+++ dataset.py
@@ -1,14 +1,31 @@
+import os
+
 from tqdm import tqdm
 import json
 
-messages = []
+messages: list[str] = []
 
-with open("messages.json", "r", encoding="utf-8") as f:
-    dataset = json.load(f)
-    for entry in tqdm(dataset, desc='Processing dataset'):
-        if entry["Contents"] is not None and entry["Contents"] != "":
-            messages.append(entry["Contents"])
 
-with open("messages.txt", "w", encoding="utf-8") as f:
-    for message in tqdm(messages, desc="Writing messages to file"):
-        f.write(message + "\n")
+def main() -> None:
+    for root, _, files in os.walk("Messages"):
+        for file in tqdm(files, desc="Iterating over files...", unit="file"):
+            if file == "messages.json":
+                print("\nDoing file: " + os.path.join(root, file))
+                with open("messages.json", "r", encoding="utf-8") as f:
+                    dataset = json.load(f)
+                    for entry in dataset:
+                        message: str = entry["Contents"] or ""
+                        if message != "":
+                            messages.append(message)
+
+    with open("messages.txt", "w", encoding="utf-8") as f:
+        if messages != []:
+            for message in tqdm(messages, desc="Writing messages to file"):
+                f.write(message + "\n")
+        else:
+            raise ValueError(
+                "You must have a non-empty list of messages in order for markovbot to work.")
+
+
+if __name__ == "__main__":
+    main()
