7e6a312bab704f3c8ec8efa2d84b3de93701eedd
bscout9956 <bs.blackscout.bs@gmail.com>
2026-04-02 14:29:46 -0300

    Add parameter for extra retries

--- markovbot.py
+++ markovbot.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from discord.abc import PrivateChannel
 from discord.ext import tasks
 import discord
@@ -12,6 +14,7 @@ handler = logging.FileHandler(
 # Token for the bot, not to be confused with the account token
 TOKEN = os.environ.get("MARKOVBOT_TOKEN", "")
 BOT_CHANNEL = int(os.environ.get("MARKOVBOT_CHANNEL_ID", 0))
+TRY_COUNT = 50
 
 
 def load_markov_model() -> markovify.NewlineText:
@@ -36,8 +39,8 @@ async def scout_thinks() -> None:
         BOT_CHANNEL)
     if channel and isinstance(channel, discord.abc.Messageable):
         generated_response = text_model.make_sentence(
-        ) or "OOC: I was unable to generate a message. Try again!"
-        message = f"BS thinks: *{generated_response}*."
+            tries=TRY_COUNT) or f"OOC: I tried {TRY_COUNT} but I was unable to generate a message for some reason..."
+        message = f"BS thinks: *{generated_response}*.\n*Next message will be sent at: {datetime.now() + timedelta(hours=1)} UTC-3*"
         await channel.send(message)
 
 
@@ -50,7 +53,7 @@ async def on_ready() -> None:
 
 @client.event
 async def on_message(message: discord.Message) -> None:
-    logging.info("Message received!")
+    logging.info(f"Message received from {message.author}.")
     if message.author == client.user:
         return
 
@@ -64,13 +67,13 @@ async def on_message(message: discord.Message) -> None:
         if len(stripped_message.split(" ")) > 2:
             await message.channel.send("OOC: You cannot have more than 2 words after the !talk command. Try again!")
         try:
-            generated_response: str = text_model.make_sentence_with_start(
-                stripped_message.replace("!talk", "").rstrip()) or "OOC: I was unable to generate a message. Try another term?"
+            generated_response: str = text_model.make_sentence_with_start(stripped_message.replace(
+                "!talk", "").rstrip(), tries=50) or f"OOC: I tried {TRY_COUNT} times and couldn't generate a message with {stripped_message}. Try another term?"
         except Exception as e:
             generated_response = f"OOC: It may not have been possible to find a chain starting with {stripped_message}.\nError: {e}"
     else:
         generated_response = text_model.make_sentence(
-        ) or "OOC: I was unable to generate a message. Try again!"
+            tries=50) or "OOC: I was unable to generate a message. Try again!"
 
     logging.info("Sending generated response!")
     await message.channel.send(generated_response)
