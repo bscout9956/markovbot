d84038ac24c0260b684e5629639fe75bda98c4b6
bscout9956 <bs.blackscout.bs@gmail.com>
2026-04-03 20:01:21 -0300

    Allow for looking up terms in random messages

--- markovbot.py
+++ markovbot.py
@@ -51,31 +51,63 @@ async def on_ready() -> None:
         scout_thinks.start()
 
 
+def random_with_lookup(look_up_term) -> str:
+    final_message = ""
+
+    tries = 0
+    while tries < TRY_COUNT and final_message != "":
+        generated_message = text_model.make_sentence(tries=10) or ""
+        if look_up_term in generated_message:
+            final_message = generated_message
+
+    return final_message
+
+
 @client.event
 async def on_message(message: discord.Message) -> None:
     logging.info(f"Message received from {message.author}.")
     if message.author == client.user:
         return
 
-    if not message.content.startswith("!talk"):
+    if message.content == "":
         return
 
+    if not message.content.startswith("!"):
+        return
+
+    isTalk = False
+    isRandomTalk = False
+
     generated_response = ""
-    stripped_message: str = message.content.replace("!talk", "").lstrip()
+    stripped_message = ""
 
-    if stripped_message != "":
-        if len(stripped_message.split(" ")) > 2:
-            await message.channel.send("OOC: You cannot have more than 2 words after the !talk command. Try again!")
-        try:
-            generated_response: str = text_model.make_sentence_with_start(stripped_message.replace(
-                "!talk", "").rstrip(), tries=50) or f"OOC: I tried {TRY_COUNT} times and couldn't generate a message with {stripped_message}. Try another term?"
-        except Exception as e:
-            generated_response = f"OOC: It may not have been possible to find a chain starting with {stripped_message}.\nError: {e}"
-    else:
-        generated_response = text_model.make_sentence(
-            tries=50) or "OOC: I was unable to generate a message. Try again!"
+    if message.content.startswith("!randomtalk"):
+        stripped_message = message.content.replace("!randomtalk", "").lstrip()
+    if message.content.startswith("!talk"):
+        stripped_message: str = message.content.replace("!talk", "").lstrip()
 
-    logging.info("Sending generated response!")
-    await message.channel.send(generated_response)
+    # TODO: DRY principle
+    if isTalk or isRandomTalk:
+        if isTalk:
+            if len(stripped_message.split(" ")) > 2:
+                await message.channel.send("OOC: You cannot have more than 2 words after the !talk command. Try again!")
+
+            try:
+                generated_response: str = text_model.make_sentence_with_start(stripped_message.replace(
+                    "!talk", "").rstrip(), tries=50) or f"OOC: I tried {TRY_COUNT} times and couldn't generate a message with {stripped_message}. Try another term?"
+            except Exception as e:
+                generated_response = f"OOC: It may not have been possible to find a chain starting with {stripped_message}.\nError: {repr(e)}"
+        if isRandomTalk:
+            if len(stripped_message.split(" ")) > 1:
+                await message.channel.send("OOC: You cannot have more than 1 word after the !randomtalk command. Try again!")
+
+            try:
+                generated_response: str = random_with_lookup(
+                    stripped_message) or f"OOC: I tried {TRY_COUNT} times and couldn't generate a random message with {stripped_message}. Try another term?"
+            except Exception as e:
+                generated_response = f"OOC: It may not have been possible to find a message containing {stripped_message} as part of the generated random message.\nError: {repr(e)}"
+
+    if generated_response != "" and (isTalk or isRandomTalk):
+        await message.channel.send(generated_response)
 
 client.run(TOKEN, log_handler=handler, root_logger=True)
