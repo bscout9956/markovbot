bf9a26a17c62f43f0f6f49d07ce71f4937df7a19
bscout9956 <bs.blackscout.bs@gmail.com>
2026-04-04 15:52:10 -0300

    Add function to check valid channel

--- markovbot.py
+++ markovbot.py
@@ -81,6 +81,17 @@ async def on_ready() -> None:
     logging.info(f"Logged in as {client.user}")
 
 
+def is_valid_channel(message: discord.Message) -> bool:
+    is_correct_channel: bool = message.channel.id == botconfig.BOT_CHANNEL
+    is_correct_forum: bool = getattr(
+        message.channel, "parent_id", None) == botconfig.BOT_CHANNEL
+
+    if not is_correct_channel and not is_correct_forum:
+        logging.error(
+            f"Message received in a channel that is not the bot channel. Message channel ID: {message.channel.id}, Bot channel ID: {botconfig.BOT_CHANNEL}")
+        return False
+
+    return True
 
 
 @client.event
