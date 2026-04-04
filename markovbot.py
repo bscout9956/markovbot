6b0dc80b7474055767582b28d23b03ad28bf0cae
bscout9956 <bs.blackscout.bs@gmail.com>
2026-04-04 15:04:06 -0300

    Print/Send out randomtalk run time and stats

--- markovbot.py
+++ markovbot.py
@@ -7,6 +7,7 @@ import markovify
 import logging
 import os
 import asyncio
+import time
 
 import botconfig
 import model_manager
@@ -51,11 +52,20 @@ def random_with_lookup(look_up_term) -> str:
     final_message = ""
     logging.info(f"Generating random message with lookup term: {look_up_term}")
     tries = 0
+    start = time.time()
     while tries < botconfig.TRY_COUNT * 10 and final_message == "":
         generated_message = text_model.make_sentence(tries=10) or ""
         if look_up_term in generated_message:
             final_message = generated_message
         tries += 1
+    end = time.time()
+
+    time_taken = end - start
+
+    logging.info(
+        f"Time taken to generate message: {time_taken:.4f} seconds with {tries} tries.")
+
+    final_message += f"\n\n*Note: It took {tries} tries and {time_taken:.4f} seconds to generate this message with the term '{look_up_term}'*"
 
     logging.info(
         f"Final message generated with lookup term {look_up_term}: {final_message}")
