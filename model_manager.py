47c3ab9f0b8bfee40c0b7687f2c79d1795cb96e1
SpiderVice <spidervice@gmail.com>
2026-04-05 11:05:34 +0100

    fix(model_manager): move those to data/ too

--- model_manager.py
+++ model_manager.py
@@ -2,17 +2,17 @@ import markovify
 
 
 def save_model(state_size: int) -> None:
-    with open("messages.txt", encoding="utf-8") as f:
+    with open("data/messages.txt", encoding="utf-8") as f:
         text: str = f.read()
 
         text_model = markovify.NewlineText(
             text, well_formed=False, state_size=state_size)
         model_json: str = text_model.to_json()
-        with open("markov_model.json", "w", encoding="utf-8") as model_file:
+        with open("data/markov_model.json", "w", encoding="utf-8") as model_file:
             model_file.write(model_json)
 
 
 def load_model() -> markovify.NewlineText:
-    with open("markov_model.json", "r", encoding="utf-8") as model_file:
+    with open("data/markov_model.json", "r", encoding="utf-8") as model_file:
         model_json: str = model_file.read()
         return markovify.NewlineText.from_json(model_json)
